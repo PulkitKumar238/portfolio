e09826e04b0b484c7c45874c9ffd52b1fedc4786
Pulkit Kumar <pulkitnishad789@gmail.com>
2024-07-24 17:40:11 +0530

    made some changes in about section

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -5,19 +5,15 @@ import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
+  SiCss3,
   SiFirebase,
   SiNextdotjs,
   SiSolidity,
-  SiPostgresql,
+
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
@@ -29,9 +25,6 @@ function Techstack() {
         <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
@@ -40,30 +33,20 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiSolidity />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
+       <Col xs={4} md={2} className="tech-icons">
+        <SiCss3/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
+     
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiJava />
       </Col>
+
     </Row>
   );
 }
